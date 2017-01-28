3f2f0c583d3cabcbbf1be8abf774aca857619fcc
Jagrut Kosti <jagrut.kosti@uni-konstanz.de>
2017-01-28 15:43:50 +0100

    Settings now updated after user input. Shows current value in summary.

--- app/src/main/java/dashit/uni/com/dashit/view/activity/SettingsActivity.java
+++ app/src/main/java/dashit/uni/com/dashit/view/activity/SettingsActivity.java
@@ -1,7 +1,10 @@
 package dashit.uni.com.dashit.view.activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 
@@ -16,25 +19,60 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsPreferenceFragment()).commit();
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment
-    {
+    public static class SettingsPreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
         @Override
         public void onCreate(final Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 
             EditTextPreference textPref = (EditTextPreference) findPreference("myName");
-            textPref.setSummary(textPref.getText());
+            if(textPref.getText() != null)
+                textPref.setSummary("Your name: " + textPref.getText());
+            else
+                textPref.setSummary("Your name: Not set");
 
             textPref = (EditTextPreference) findPreference("myPhoneNumber");
-            textPref.setSummary(textPref.getText());
+            if(textPref.getText() != null)
+                textPref.setSummary("Your phone number: " + textPref.getText());
+            else
+                textPref.setSummary("Your phone number: Not set");
 
             textPref = (EditTextPreference) findPreference("contact");
-            textPref.setSummary(textPref.getText());
+            if(textPref.getText() != null)
+                textPref.setSummary("Your emergency contact: " + textPref.getText());
+            else
+                textPref.setSummary("Your emergency contact: Not set");
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            EditTextPreference textPref = (EditTextPreference) findPreference(key);
+            String title = (String) textPref.getTitle();
+            switch(title){
+                case "Name":
+                    if(textPref.getText() != null)
+                        textPref.setSummary("Your name: " + textPref.getText());
+                    else
+                        textPref.setSummary("Your name: Not set");
+                    break;
+                case "Phone Number":
+                    if(textPref.getText() != null)
+                        textPref.setSummary("Your phone number: " + textPref.getText());
+                    else
+                        textPref.setSummary("Your phone number: Not set");
+                    break;
+                case "Emergency Contact":
+                    if(textPref.getText() != null)
+                        textPref.setSummary("Your emergency contact: " + textPref.getText());
+                    else
+                        textPref.setSummary("Your emergency contact: Not set");
+                    break;
+            }
         }
     }
 }
